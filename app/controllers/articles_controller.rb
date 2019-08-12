dfde065a531dbde2237049275d97750f45169d5d
Anjana <anjana.enbake@gmail.com>
2019-08-12 18:16:48 +0530

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,10 @@ class ArticlesController < ApplicationController
 	  @article = Article.new
 	end
 
+    def edit
+      @article = Article.find(params[:id])
+    end
+     
 	def create
 	  @article = Article.new(article_params)
 	   
@@ -19,6 +23,23 @@ class ArticlesController < ApplicationController
 	  end
 	end
 
+    def update
+      @article = Article.find(params[:id])
+     
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+     
+      redirect_to articles_path
+    end
+    
 	private
 	  def article_params
 	    params.require(:article).permit(:title, :text)
